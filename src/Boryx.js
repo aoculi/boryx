122a6ed9e7b8e1211874885956b41da6f130f07e
Alexis <alexis.oculi@gmail.com>
2022-02-19 12:46:34 +0100

    refactor: code format

--- src/Boryx.js
+++ src/Boryx.js
@@ -1,55 +1,55 @@
 import { createServer } from 'http'
 
 export default class Boryx {
-	#routes = []
-	#options = null
-	#server = null
+  #routes = []
+  #options = null
+  #server = null
 
-	constructor(opts = { port: 3000 }) {
-		this.#options = opts
-	}
+  constructor(opts = { port: 3000 }) {
+    this.#options = opts
+  }
 
-	get(path, callback) {
-		this.#routes.push({
-			method: 'GET',
-			path,
-			callback,
-		})
-	}
+  get(path, callback) {
+    this.#routes.push({
+      method: 'GET',
+      path,
+      callback,
+    })
+  }
 
-	list() {
-		return this.#routes
-	}
+  list() {
+    return this.#routes
+  }
 
-	createServer() {
-		this.#server = createServer((req, res) => {
-			res.setHeader('Content-Type', 'application/json')
-			res.writeHead(200)
+  createServer() {
+    this.#server = createServer((req, res) => {
+      res.setHeader('Content-Type', 'application/json')
+      res.writeHead(200)
 
-			// find the good route
-			const route = this.#routes.find(
-				(r) => r.path === req.url && r.method === req.method
-			)
+      // find the good route
+      const route = this.#routes.find(
+        (r) => r.path === req.url && r.method === req.method
+      )
 
-			// return error when route not found
-			if (!route) return res.end(JSON.stringify({ error: 'Invalid request!' }))
+      // return error when route not found
+      if (!route) return res.end(JSON.stringify({ error: 'Invalid request!' }))
 
-			// execute route callback
-			return route.callback(req, res)
-		})
+      // execute route callback
+      return route.callback(req, res)
+    })
 
-		return this.#server
-	}
+    return this.#server
+  }
 
-	run(callback = null) {
-		if (!this.#server) this.createServer()
+  run(callback = null) {
+    if (!this.#server) this.createServer()
 
-		this.#server.listen(this.#options.port)
+    this.#server.listen(this.#options.port)
 
-		if (callback) callback(this.#options)
-	}
+    if (callback) callback(this.#options)
+  }
 
-	async close() {
-		this.#server.close()
-	}
+  async close() {
+    this.#server.close()
+  }
 }
